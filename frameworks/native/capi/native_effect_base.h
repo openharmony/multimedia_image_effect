26976c843ff81626d4ee5832a5fbcf4a209dd265
s00887906 <shuhuan3@huawei.com>
2025-03-18 18:58:14 +0800

    AI扫描，内存泄漏修复
    Signed-off-by: s00887906 <shuhuan3@huawei.com>

--- frameworks/native/capi/native_effect_base.h
+++ frameworks/native/capi/native_effect_base.h
@@ -42,7 +42,7 @@ struct OH_ImageEffect {
     ~OH_ImageEffect()
     {
         if (saveJson != nullptr) {
-            delete saveJson;
+            delete[] saveJson;
             saveJson = nullptr;
         }
         for (const auto &filter : filters_) {
