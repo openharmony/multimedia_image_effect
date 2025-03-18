a3a99c44a852ca3db2c9eef3c48d74ab1d3d377f
cenfarm <cen1920@163.com>
2025-03-18 11:42:11 +0800

    稳定性判空问题3
    Signed-off-by: cenfarm <cen1920@163.com>

--- frameworks/native/effect/base/image_effect_inner.cpp
+++ frameworks/native/effect/base/image_effect_inner.cpp
@@ -718,7 +718,8 @@ ErrorCode ImageEffect::GetImageInfoFromPicture(uint32_t &width, uint32_t &height
     std::shared_ptr<ExifMetadata> &exifMetadata) const
 {
     std::shared_ptr<PixelMap> pixelMap = inDateInfo_.picture_->GetMainPixel();
-    CHECK_AND_RETURN_RET_LOG(pixelMap != nullptr, ErrorCode::ERR_CREATE_IMAGESOURCE_FAIL, "GetMainPixel fail!");
+    CHECK_AND_RETURN_RET_LOG(pixelMap != nullptr, ErrorCode::ERR_CREATE_PIXELMAP_FAIL,
+        "GetImageInfoFromPicture pixelMap is nullptr!");
     width = static_cast<uint32_t>(pixelMap->GetWidth());
     height = static_cast<uint32_t>(pixelMap->GetHeight());
     pixelFormat = pixelMap->GetPixelFormat();
