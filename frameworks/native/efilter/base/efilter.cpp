87f519569ddc04a01785bb30be146ca6a8cd25b4
m30063213 <m30063213@notesmail.huawei.com>
2025-07-24 11:30:17 +0800

    稳定性问题+2 Signed-off-by: maqianli <15735184237@163.com>

--- frameworks/native/efilter/base/efilter.cpp
+++ frameworks/native/efilter/base/efilter.cpp
@@ -107,6 +107,10 @@ ErrorCode EFilter::PreRender(IEffectFormat &format)
 std::shared_ptr<PixelFormatCap> GetPixelFormatCap(std::string &name)
 {
     std::shared_ptr<PixelFormatCap> pixelFormatCap = std::make_shared<PixelFormatCap>();
+    if (pixelFormatCap == nullptr) {
+        EFFECT_LOGE("GetPixelFormatCap: PixelFormatCap fail! name=%{public}s", name.c_str());
+        return pixelFormatCap;
+    }
     std::shared_ptr<EffectInfo> effectInfo = EFilterFactory::Instance()->GetEffectInfo(name);
     if (effectInfo == nullptr) {
         EFFECT_LOGE("GetPixelFormatCap: GetEffectInfo fail! name=%{public}s", name.c_str());
